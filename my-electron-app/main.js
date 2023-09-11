486ceec27d9199cb29389c7fdb3e1367cfebef9a
Ashish <himanashish446@gmail.com>
2023-09-11 16:15:15 +0530

    message

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08eb94b484bcbf2fddef1649c417c3b6648360fd336a7960f13f19554a65cfe3
-size 1749
+oid sha256:b9be9a568867f4d0a7e17914e8eb16b8697dcc25a8dfa33f960f536e0da9d7fa
+size 1853
